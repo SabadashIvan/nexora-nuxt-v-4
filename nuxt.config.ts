1ca3e2a5551def8d5890450207fb904d3d43209f
Ivan <isabadash48@gmail.com>
2025-12-15 17:47:08 +0200

    Add Nuxt configuration for core modules

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/hints'
+  ]
+})
